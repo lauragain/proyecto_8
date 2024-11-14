1117502b3f7996a22ef638cec1470b55f05e27f8
Laura <laugain8@gmail.com>
2024-11-14 17:13:48 +0100

    error handler

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.use('/api', require('./routes/api.routes'))
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).json(err);
+})
+
+
 module.exports = app
